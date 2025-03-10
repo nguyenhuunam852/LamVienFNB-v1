aeb2b99d8c480eecb34cf123be8b2aedbae66640
Huu Nam Nguyen <huunam.nguyen@planet1world.com>
2025-03-11 00:38:30 +0700

    first init

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.vue"],
+    languageOptions: {parserOptions: {parser: tseslint.parser}},
+    rules: {
+    indent: ['error', 2],
+    quotes: ['warn', 'single'],
+    semi: ['warn', 'never'],
+    'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: true }],
+    'comma-dangle': ['warn', 'always-multiline'],
+    'vue/multi-word-component-names': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/no-v-html': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/html-self-closing': [
+      'warn',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always',
+        },
+      },
+    ],
+    'vue/no-v-text-v-html-on-component': 'off',
+    },
+  },
+];
